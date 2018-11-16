84f054ac16d294bfd22a23ea92455ce3f987f2e1
Jon Shaw <Jonshaw@quickenloans.com>
2018-11-16 15:52:34 -0500

    cosmetic updates again, making things look pretty

--- using.js
+++ using.js
@@ -1,30 +1,22 @@
 function rightAnswer1(){
     sleep(3000);
-    //document.getElementById("answer-1").innerHTML = "correct";
-    alert("That is correct");
     resetAera("answer-1");
 }
 function rightAnswer2(){
     sleep(3000);
-    //document.getElementById("answer-2").innerHTML = "correct";
-    alert("That is correct");
     resetAera("answer-2");
 }
 function rightAnswer3(){
     sleep(3000);
-    //document.getElementById("answer-3").innerHTML = "correct";
-    alert("That is correct");
     resetAera("answer-3");
 }
 function rightAnswer4(){
     sleep(3000);
-    //document.getElementById("answer-4").innerHTML = "correct";
-    alert("That is correct");
     resetAera("answer-4");
 }
 function wrongAnswer1(){
     sleep(3000);
-    document.getElementById("answer-1").innerHTML = "Wrong";
+    document.getElementById("answer-1").innerHTML = "Wrong - Try again";
 }
 function wrongAnswer2(){
     sleep(3000);
@@ -32,16 +24,16 @@ function wrongAnswer2(){
 }
 function wrongAnswer3(){
     sleep(3000);
-    document.getElementById("answer-3").innerHTML = "Wrong";
+    document.getElementById("answer-3").innerHTML = "Wrong - Try again";
 }
 function wrongAnswer4(){
     sleep(3000);
-    document.getElementById("answer-4").innerHTML = "Wrong";
+    document.getElementById("answer-4").innerHTML = "Wrong - Try again";
 }
 
 function resetAera(x){
     sleep(3000);
-    document.getElementById( x).innerHTML = "Next Question";
+    document.getElementById( x).innerHTML = "Correct! - Next Question";
     
 }
 
